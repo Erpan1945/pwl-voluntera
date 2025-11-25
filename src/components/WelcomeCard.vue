155788146f1b90b93bef387d2f3c9d91e5c77e76
Kezia Sihombing <keziasihombing184@gmail.com>
2025-11-25 14:51:27 +0700

    adding welcomeCard component for dashboard

---
+++ src/components/WelcomeCard.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="w-full max-w-9/10 !mx-auto p-5 rounded-lg border border-black/10 !mt-6" style="background-color: white; border: 1px solid rgba(0, 0, 0, 0.10); box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.10), 0 1px 2px -1px rgba(0, 0, 0, 0.10);">
+        <h1 class="text text-black !text-2xl md:!text-xl !mb-2">Selamat Datang, Budi Santoso!</h1>
+        <p class="text text-[#4A5565] !text-base md:!text-sm">Temukan kegiatan relawan yang sesuai dengan minat </p>
+    </div>
+</template>
